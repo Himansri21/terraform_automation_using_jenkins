27509509037b61fdc2e7e98fae88d8b1684d5a2b
Himanshu <137988128+Himansri21@users.noreply.github.com>
2025-04-09 20:09:20 +0530

    Update main.tf

--- custom-fe-be/main.tf
+++ custom-fe-be/main.tf
@@ -1,3 +1,13 @@
+terraform {
+  backend "s3" {
+    bucket         = "your-terraform-state-bucket"
+    key            = "path/to/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
+  }
+}
+
 ##############################
 # VARIABLES & DATA SOURCES
 ##############################
